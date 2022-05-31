0fbef11876fb00d7fdc5521f438b754c615a069b
CECTPAYTKYMHE <bugatti282@gmail.com>
2022-05-31 16:52:23 +0300

    last

--- makeiteasy/views.py
+++
@@ -1,7 +0,0 @@
-﻿from django.shortcuts import render
-
-def home(request):
-    context = {
-        'title': 'Главная'
-    }
-    return render(request, 'main/home.html', context)
--- pdf/urls.py
+++
@@ -1,9 +0,0 @@
-﻿from django.urls import path
-from .views import Pdftojpg, Jpgtopdf, Pdftotxt
-
-
-urlpatterns = [
-    path('pdftojpg/', Pdftojpg.as_view(), name='pdf'),
-    path('jpgtopdf/', Jpgtopdf.as_view(), name='jpg'),
-    path('pdftotxt/', Pdftotxt.as_view(), name='txt'),
-]
